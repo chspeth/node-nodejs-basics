3c7992c8d1130910c2f525e10e1fbc8d7112bfec
Валерия <sp.ether@yandex.ru>
2023-06-25 00:36:10 +0200

    feat: cli & modules are ready

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+const neededArguments = process.argv;
+const arr = [];
+
 const parseArgs = () => {
-    // Write your code here 
+    for (let i = 0; i < neededArguments.length; i++) {
+        if (neededArguments[i].startsWith("--")) {
+            arr.push(neededArguments[i].slice(2) + " is " + neededArguments[i+1])
+        }
+    }
+    console.log(arr.join(", "));
 };
 
 parseArgs();

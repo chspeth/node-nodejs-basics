3a2263c427e000188acf2d5c88a75603c5621bfb
Валерия <sp.ether@yandex.ru>
2023-06-27 19:39:16 +0200

    feat: zip & wt are ready

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { getPath } from '../util.js';
+import { Worker } from 'worker_threads';
+import { availableParallelism } from 'os';
+
+const INITIAL_NUMBER = 10;
+
+const workerFile = getPath(import.meta.url, 'worker.js');
+
+const getWorkerService = (num) => new Promise(resolve => {
+    const worker = new Worker(workerFile, { 
+        workerData: num
+    });
+
+    worker.on('message', data => resolve({
+        status: 'resolved',
+        data
+    }));
+
+    worker.on('error', () => resolve({
+        status: 'error',
+        data: null
+    }));
+})
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPool = Array.from({ length: availableParallelism() }, (_, i) => getWorkerService(INITIAL_NUMBER + i));
+    const result = await Promise.all(workerPool);
+    console.log(result);
 };
 
 await performCalculations();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { getPath } from '../util.js';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const fileToDecompress = getPath(import.meta.url, 'files/archive.gz');
+const unzippedFile = getPath(import.meta.url, 'files/fileToCompress.txt');
+
+const readStream = createReadStream(fileToDecompress);
+const unzipStream = createGunzip();
+const writeStream = createWriteStream(unzippedFile);
+
 const decompress = async () => {
-  // Write your code here 
+  pipeline(
+    readStream,
+    unzipStream,
+    writeStream,
+    (err) => console.log(err)
+  )
 };
 
 await decompress();
